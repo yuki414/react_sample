45cb1302cc33dddf5f12ca968b93848f51f67544
Yuki Uchiyama <uchiyama.yuki@moneyforward.co.jp>
2022-02-09 13:04:12 +0900

    Send props from board component to square component

--- src/index.js
+++ src/index.js
@@ -12,10 +12,8 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button
-        className="square"
-        onClick={() => { this.setState({ value: 'X' }) }}>
-        {this.state.value}
+      <button className="square">
+        {this.props.value}
       </button>
     );
   }
